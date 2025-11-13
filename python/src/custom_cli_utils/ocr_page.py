08f7171e23d780fa91c3f7510af7a5648839ce40
MyName <delanoleslieprof@gmail.com>
2025-11-12 17:57:00 -0800

    ocr_page python util

--- python/src/custom_cli_utils/ocr_page.py
+++ python/src/custom_cli_utils/ocr_page.py
@@ -11,11 +11,11 @@ def main():
     args = parser.parse_args()
     
     filename = args.filename
-    base_file_name = os.path.basename(filename).split(".")[:-2]
+    base_file_name = ".".join(os.path.basename(filename).split(".")[:-1])
     page_index = int(args.page)
 
     start = page_index - 1
-    end = page_index
+    end = page_index - 1
 
     os.system(f'mineru -b vlm-transformers -p {filename} -s {start} -e {end} -o /tmp/ && cat /tmp/{base_file_name}/vlm/{base_file_name}.md')
 
